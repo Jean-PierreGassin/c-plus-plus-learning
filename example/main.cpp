3d727496517251a158d5ec50ffa813723ea58d8d
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-05-18 22:00:03 +1000

    Initial Commit

---
+++ example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  example
+//
+//  Created by Jean-Pierre Gassin on 18/5/18.
+//  Copyright © 2018 Jean-Pierre Gassin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
